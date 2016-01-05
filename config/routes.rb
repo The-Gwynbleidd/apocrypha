03ebebde9829502a0e3dbc1cf452f174db0532b2
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-01-05 16:36:06 +0100

    Add Static pages. Add Puma for deployment server

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  # Static Pages #
+  get 'index' =>'static_pages#index'
+  get 'about' => 'static_pages#about'
+
   # Categories #
   resources :categories
-  
+
   # Root page #
-  root "categories#index"
+  root "static_pages#index"
 
 end
